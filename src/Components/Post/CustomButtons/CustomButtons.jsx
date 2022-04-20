7b4033a50cec58f853d5433eacfa5578fdca3a00
michael-prye <michael.prye@gmail.com>
2022-04-20 15:56:59 -0400

    Added functionality to thumbs up & down button + Created Custom Button component and stypeSheet for button

---
+++ src/Components/Post/CustomButtons/CustomButtons.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./CustomButton.css"
+
+const CustomButtons = (props) => {
+    const [thumbUpButton, setThumbUpButton] = useState('inactive')
+    const [thumbDownButton, setThumbDownButton] = useState('inactive')
+
+    function thumbUpClick(){
+        if(thumbUpButton === 'inactive'){
+            setThumbUpButton('thumb-up')
+            setThumbDownButton('inactive')
+        }
+        
+    }
+    function thumbDownClick(){
+        if(thumbDownButton ==='inactive'){
+            setThumbDownButton('thumb-down')
+            setThumbUpButton('inactive')
+        }
+
+    }
+    return ( 
+        <div>
+            <button className={thumbUpButton} onClick={thumbUpClick}>THUMBS UP</button>
+            <button className={thumbDownButton} onClick={thumbDownClick}>THUMBS DOWN</button>
+            </div>
+     );
+}
+ 
+export default CustomButtons;
