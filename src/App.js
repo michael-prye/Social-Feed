f6c151f61126744398fba6cb04e04e9da3b028b9
michael-prye <michael.prye@gmail.com>
2022-04-20 15:11:25 -0400

    Created handleSubmit function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import DisplayPosts from "./Components/DisplayPosts/DisplayPosts";
+import CreatePost from "./Components/CreatePost/CreatePost";
 
 
 
@@ -8,9 +9,15 @@ function App() {
   {name: 'Steve',post: 'faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam' }, 
   {name: 'Michael', post: 'faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam'}, 
   {name: 'Bob', post: 'faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam'}]);
+
+  function addNewPost(entry){
+    let tempPosts = [...posts, entry];
+    setPosts(tempPosts)
+  }
   
   return (
     <div>
+      <CreatePost addNewPost={addNewPost}/>
       <DisplayPosts posts = {posts}/>
     </div>
   );
--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -4,13 +4,23 @@ const CreatePost = (props) => {
     const [name, setName] = useState('');
     const [post, setPost] = useState('');
 
+    function handleSubmit(event){
+        event.preventDefault();
+        let newPost = {
+            name: name,
+            post: post
+        };
+        props.addNewPost(newPost)
+    }
+
 
     return ( 
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Name</label>
             <input type='text' value={name} onChange={(event)=> setName(event.target.value)}/>
             <label>Post</label>
             <input type="text" value={post} onChange={(event)=> setPost(event.target.value)} />
+            <button type="submit">Create</button>
         </form>
      );
 }
