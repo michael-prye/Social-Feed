a5d14e6ed8701e3c9932e1160d4770963b968a1e
michael-prye <michael.prye@gmail.com>
2022-04-22 16:16:51 -0400

    Updated Post button to images

--- src/Components/Post/CustomButtons/CustomButtons.jsx
+++ src/Components/Post/CustomButtons/CustomButtons.jsx
@@ -7,31 +7,42 @@ import "./CustomButton.css"
 const CustomButtons = (props) => {
     const [thumbUpButton, setThumbUpButton] = useState('inactive')
     const [thumbDownButton, setThumbDownButton] = useState('inactive')
+    const [upImg, setUpImg] = useState(require('./ThumbsUpGray.png'))
+    const [downImg, setDownImg] = useState(require('./ThumbsDownGrey.png'))
+    
 
     function thumbUpClick(){
         if(thumbUpButton === 'inactive'){
             setThumbUpButton('thumb-up')
+            setUpImg(require('./ThumbsUpGreen.png'))
             setThumbDownButton('inactive')
+            setDownImg(require('./ThumbsDownGrey.png'))
         }
         else{
             setThumbUpButton('inactive')
+            setUpImg(require('./ThumbsUpGray.png'))
         }
         
     }
     function thumbDownClick(){
         if(thumbDownButton ==='inactive'){
             setThumbDownButton('thumb-down')
+            setDownImg(require('./ThumbsDownRed.png'))
             setThumbUpButton('inactive')
+            setUpImg(require('./ThumbsUpGray.png'))
         }
         else{
             setThumbDownButton('inactive')
+            setDownImg(require('./ThumbsDownGrey.png'))
         }
 
     }
+
     return ( 
         <div class="button">
-            <button className={thumbUpButton} onClick={thumbUpClick}>THUMBS UP</button>
-            <button className={thumbDownButton} onClick={thumbDownClick}>THUMBS DOWN</button>
+            <img className={thumbUpButton} src={upImg} onClick={thumbUpClick} alt=''/>
+            <img className={thumbDownButton} src={downImg} onClick={thumbDownClick} alt=''/>
+            
 
             </div>
      );
